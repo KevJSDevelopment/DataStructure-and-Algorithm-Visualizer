194f6d91b6c2740b627904d8123323d3cf3aad25
KevJSDevelopment <Kshhn12@gmail.com>
2021-01-26 12:13:29 -0500

    spaced out nav & content

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
+import Navbar from './components/Navbar'
+import Algorithms from './containers/Algorithms'
+
 function App() {
   return (
     <div className="App">
-      <h1 className="text-red-500">
-        hello
-      </h1>
+      <div class="grid grid-cols-3 gap-4">
+        <div class="col-span-1 ..."><Navbar /></div>
+        <div class="col-span-2 ..."><Algorithms/></div>
+      </div>
     </div>
   );
 }
