9151bde5021d7b4327e055239434f40a445cab8a
KevJSDevelopment <Kshhn12@gmail.com>
2021-01-28 14:05:53 -0500

    added links for navigation

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,13 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Home = () => {
     return (
         <div>
-            
+            Homepage
+            <button className="bg-gray-500 rounded-md ml-6 hover:bg-gray-300">
+                <Link to="/multiple-pointers">Multiple pointers</Link>
+            </button>
         </div>
     )
 }
