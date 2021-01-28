85d056dcd8f3aeb5681bc1f6b765f23988e59575
KevJSDevelopment <Kshhn12@gmail.com>
2021-01-28 13:01:27 -0500

    problem animations working

--- src/components/ArrayCircle.js
+++ src/components/ArrayCircle.js
@@ -3,7 +3,7 @@ import React from 'react'
 const ArrayCircle = (props) => {
     return (
         <div className="...">
-            <div className={`bg-gray-500 rounded-full mx-auto`} style={{width: `${props.value}px`, height: `${props.value}px`, textAlign: "center"}}>
+            <div id={`circle-${props.index}`} className={`bg-gray-500 rounded-full mx-auto`} style={{width: `${props.value}px`, height: `${props.value}px`, textAlign: "center"}}>
                 {props.value}
             </div>
         </div>
--- src/components/MultiplePointers.js
+++ src/components/MultiplePointers.js
@@ -20,22 +20,42 @@ const MultiplePointers = () => {
         return Math.floor(Math.random() * (max - min + 1) + min)
     }
 
-    const findSum = (arr, total = 100) => {
-        let left = 0;
-        let right = arr.length - 1;
-        while(left < right) {
-            let sum = arr[left] + arr[right]
-            if(sum === total) {
-                 console.log([arr[left], arr[right]])
-                 return true
-            } else if(sum > 0) {
-                  right --;     
-            } else {
-                  left ++;       
-            }
+    const findSum = (left = 0, right = testArray.length - 1, total = 100, oldTarget1 = null, oldTarget2 = null) => {
+        
+        if(left >= testArray.length - 2){
+            return false
         }
-        console.log("false")
-        return false
+
+        let sum = testArray[left] + testArray[right]
+
+        if(oldTarget1 !== null){
+            oldTarget1.style.border = "0"
+        }
+        if(oldTarget2 !== null){
+            oldTarget2.style.border = "0"
+        }
+
+        const target1 = document.getElementById(`circle-${left}`)
+        const target2 = document.getElementById(`circle-${right}`)
+
+        target1.style.border = "2px"
+        target2.style.border = "2px"
+
+        target1.style.border = "2px solid black"
+        target2.style.border = "2px solid black"
+
+        if(sum === total) {
+            const answer1 = document.getElementById(`circle-${left}`)
+            const answer2 = document.getElementById(`circle-${right}`)
+
+            answer1.style.backgroundColor = "green"
+            answer2.style.backgroundColor = "green"
+            return true
+        } else if(right <= left + 1){
+            setTimeout(() => findSum(left + 1, testArray.length - 1, total, target1, target2), 200)
+        } else{
+            setTimeout(() => findSum(left, right - 1, total, target1, target2), 200)       
+        }
     }
 
     useEffect(() => {
@@ -44,12 +64,12 @@ const MultiplePointers = () => {
 
     return (
         <div>
-            <div className={`flex items-center h-64 grid grid-rows-${rows} grid-flow-col gap-8`}>
+            <div className={`flex items-center h-64 grid grid-cols-${rows} grid-flow-row gap-8`}>
                 {testArray.map((value, index) => {
                     return <ArrayCircle value={value} index={index} />
                 })}
             </div>
-            <button className="hover:bg-gray-200 rounded-xl p-1 float-right" onClick={() => findSum(testArray)}>
+            <button className="hover:bg-gray-200 rounded-xl p-1 float-right" onClick={() => findSum()}>
                 Run
             </button>
         </div>
