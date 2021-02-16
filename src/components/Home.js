14217f4cf2120bdea5cf7a0e4ec50aa36eb97cf9
KevJSDevelopment <Kshhn12@gmail.com>
2021-02-16 10:30:06 -0500

    home links fixxed

--- src/components/Home.js
+++ src/components/Home.js
@@ -12,7 +12,7 @@ const Home = () => {
                 <Link to="/merge-sort">Merge Sort</Link>
             </button>
             <button className="bg-gray-500 rounded-md ml-6 hover:bg-gray-300">
-                <Link to="/quick-pointers">Quick Sort</Link>
+                <Link to="/quick-sort">Quick Sort</Link>
             </button>
             <button className="bg-gray-500 rounded-md ml-6 hover:bg-gray-300">
                 <Link to="/bubble-sort">Bubble Sort</Link>
