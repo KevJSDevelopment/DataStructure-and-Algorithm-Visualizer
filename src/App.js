0a0724d1a51ff8328726bbccaf58ae0f3fa14a01
KevJSDevelopment <Kshhn12@gmail.com>
2021-02-24 10:33:29 -0500

    updated spacing

--- src/App.js
+++ src/App.js
@@ -1,27 +1,13 @@
 import Navbar from './containers/Navbar'
 import Algorithms from './containers/Algorithms'
-import MultiplePointers from './components/MultiplePointers'
-import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
-import Home from './components/Home';
-import MergeSort from './components/MergeSort';
-import QuickSort from './components/QuickSort';
-import BubbleSort from './components/BubbleSort';
-import LinkedList from './components/SingleLinkedList';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 const App = () => {
   return (
-    <div className="App h-screen">
+    <div className="App h-screen flex flex-row">
       <Router>
         <Navbar />
-        <Switch>
-            <Route path="/" exact render={() => <Home /> }/>
-            <Route path="/multiple-pointers" exact render={() => <MultiplePointers /> }/> 
-            <Route path="/merge-sort" exact render={() => <MergeSort /> }/> 
-            <Route path="/quick-sort" exact render={() => <QuickSort /> }/> 
-            <Route path="/bubble-sort" exact render={() => <BubbleSort /> }/> 
-            <Route path="/linked-list" exact render={() => <LinkedList /> }/>
-            <Redirect to="/"/>
-        </Switch>
+        <Algorithms />
       </Router>
     </div>
   );
--- src/containers/Navbar.js
+++ src/containers/Navbar.js
@@ -4,20 +4,20 @@ import { Link } from 'react-router-dom'
 const Navbar = () => {
 
     return (
-        <div className="w-screen flex text-center bg-black mb-6 p-4 shadow-xl">
-            <button className="bg-gray-500 rounded-md ml-40 hover:bg-gray-100">
+        <div className="h-screen w-32 text-center bg-black mb-6 p-4 shadow-xl">
+            <button className="bg-gray-500 rounded-md hover:bg-gray-100">
                 <Link to="/multiple-pointers">Multiple pointers</Link>
             </button>
-            <button className="bg-gray-500 rounded-md ml-40 hover:bg-gray-100">
+            <button className="bg-gray-500 rounded-md hover:bg-gray-100">
                 <Link to="/merge-sort">Merge Sort</Link>
             </button>
-            <button className="bg-gray-500 rounded-md ml-40 hover:bg-gray-100">
+            <button className="bg-gray-500 rounded-md hover:bg-gray-100">
                 <Link to="/quick-sort">Quick Sort</Link>
             </button>
-            <button className="bg-gray-500 rounded-md ml-40 hover:bg-gray-100">
+            <button className="bg-gray-500 rounded-md hover:bg-gray-100">
                 <Link to="/bubble-sort">Bubble Sort</Link>
             </button>
-            <button className="bg-gray-500 rounded-md ml-40 hover:bg-gray-100">
+            <button className="bg-gray-500 rounded-md hover:bg-gray-100">
                 <Link to="/linked-list">Singly Linked List</Link>
             </button>
         </div>
