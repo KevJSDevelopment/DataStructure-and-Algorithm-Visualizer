368bce02f92b266385e8c096e6924eb974e906d7
KevJSDevelopment <Kshhn12@gmail.com>
2021-03-07 10:06:16 -0500

    priority queue & weighted graph classes created

--- src/containers/Dijkstras.js
+++ src/containers/Dijkstras.js
@@ -1,5 +1,34 @@
 import React from 'react'
 
+class WeightedGraph {
+    constructor() {
+        this.adjacencyList = {};
+    }
+    addVertex(vertex){
+        if(!this.adjacencyList[vertex]) this.adjacencyList[vertex] = [];
+    }
+    addEdge(vertex1,vertex2, weight){
+        this.adjacencyList[vertex1].push({node:vertex2, weight});
+        this.adjacencyList[vertex2].push({node:vertex1, weight});
+    }
+}
+
+class PriorityQueue {
+    constructor(){
+      this.values = [];
+    }
+    enqueue(val, priority) {
+      this.values.push({val, priority});
+      this.sort();
+    };
+    dequeue() {
+      return this.values.shift();
+    };
+    sort() {
+      this.values.sort((a, b) => a.priority - b.priority);
+    };
+}
+
 const Dijkstras = () => {
     return (
         <div>
